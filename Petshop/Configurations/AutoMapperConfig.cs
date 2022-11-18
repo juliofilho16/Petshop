8bafa276e27acf6fbebbf58eaab7e2acce36bf5f
juliofilho16 <filhojulio28@gmail.com>
2022-11-18 10:03:56 -0300

    incluindo injeção de dependencia para repositorios e configurando auto mapper

--- Petshop/Configurations/AutoMapperConfig.cs
+++ Petshop/Configurations/AutoMapperConfig.cs
@@ -1,6 +1,35 @@
-﻿namespace Petshop.Configurations
+﻿using AutoMapper;
+using Petshop.Borders.Mappings;
+using Petshop.Entities.EntitiesContext;
+using Produtoshop.Borders.Mappings;
+
+namespace Petshop.Api.Configurations
 {
     public static class AutoMapperConfig
     {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new CategoriaProfile());
+                cfg.AddProfile(new CidadeProfile());
+                cfg.AddProfile(new ClienteProfile());
+                cfg.AddProfile(new EnderecoProfile());
+                cfg.AddProfile(new EspecieProfile());
+                cfg.AddProfile(new EstadoProfile());
+                cfg.AddProfile(new FuncionarioProfile());
+                cfg.AddProfile(new PagamentoProfile());
+                cfg.AddProfile(new PessoaProfile());
+                cfg.AddProfile(new PetProfile());
+                cfg.AddProfile(new ProdutoProfile());
+                cfg.AddProfile(new RacaProfile());
+                cfg.AddProfile(new ServicoProdutoProfile());
+                cfg.AddProfile(new ServicoProfile());
+                
+            });
+            var mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+        }
     }
 }
--- Petshop/Configurations/RepositoriesConfig.cs
+++ Petshop/Configurations/RepositoriesConfig.cs
@@ -1,12 +1,25 @@
-﻿
+﻿using Petshop.Borders.Mappings;
+using Petshop.Borders.Repositories.Contracts;
+using Petshop.Repositories;
 
-namespace Petshop.Configurations
+namespace Petshop.Api.Configurations
 {
     public class RepositoriesConfig
     {
         public static void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<ICidadeRepository, CidadeRepository>();
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IEspecieRepository, EspecieRepository>();
+            services.AddScoped<IFuncionarioRepository, FuncionarioRepository>();
+            services.AddScoped<IPagamentoRepository, PagamentoRepository>();
+            services.AddScoped<IPessoaRepository, PessoaRepository>();
+            services.AddScoped<IPetRepository, PetRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IRacaRepository, RacaRepository>();
+            services.AddScoped<IServicoRepository, ServicoRepository>();
         }
     }
 }
