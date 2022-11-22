cc4466562b1a9a2f57a41d2664987f3f2cbe2ac7
juliofilho16 <filhojulio28@gmail.com>
2022-11-22 13:23:22 -0300

    implementação use case busca serviço

---
+++ Petshop.UseCases/Servico/GetListServicosUseCase.cs
@@ -0,0 +1,37 @@
+﻿using Petshop.Borders.Dtos.Repositories.Servico;
+using Petshop.Borders.Repositories.Contracts;
+using Petshop.Borders.Shared;
+using Petshop.Borders.UseCase.Servico;
+
+namespace Petshop.UseCases.Servico
+{
+    public class GetListServicosUseCase : IGetListServicosUseCase
+    {
+        private readonly IServicoRepository _servicoRepository;
+
+        public GetListServicosUseCase(IServicoRepository ServicoRepository)
+        {
+            _servicoRepository = ServicoRepository;
+        }
+
+        public UseCaseResponse<List<ReadServicoDto>> Execute(UseCaseRequest<int> request)
+        {
+            var response = new UseCaseResponse<List<ReadServicoDto>>();
+
+            try
+            {
+                if (request == null)
+                    throw new ArgumentNullException(nameof(request));
+
+                var list = _servicoRepository.GetListServicos();
+                response.SetResult(list);
+            }
+            catch (Exception ex)
+            {
+                response.SetError(ex.Message, null);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Petshop.UseCases/Servico/GetServicoUseCase.cs
@@ -0,0 +1,37 @@
+﻿using Petshop.Borders.Dtos.Repositories.Servico;
+using Petshop.Borders.Repositories.Contracts;
+using Petshop.Borders.Shared;
+using Petshop.Borders.UseCase.Servico;
+
+namespace Petshop.UseCases.Servico
+{
+    public class GetServicoUseCase : IGetServicoUseCase
+    {
+        private readonly IServicoRepository _servicoRepository;
+
+        public GetServicoUseCase(IServicoRepository ServicoRepository)
+        {
+            _servicoRepository = ServicoRepository;
+        }
+
+        public UseCaseResponse<ReadServicoDto> Execute(UseCaseRequest<int> request)
+        {
+            var response = new UseCaseResponse<ReadServicoDto>();
+
+            try
+            {
+                if (request == null)
+                    throw new ArgumentNullException(nameof(request));
+
+                var ServicoResult = _servicoRepository.GetServico(request.RequestValue);
+                response.SetResult(ServicoResult);
+            }
+            catch (Exception ex)
+            {
+                response.SetError(ex.Message, null);
+            }
+
+            return response;
+        }
+    }
+}
